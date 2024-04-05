52a4345384f181f79c9fe164d2a730d69d975abf
sandhya <sandhya@sandhyas-Mac-mini.local>
2024-04-05 13:03:18 +0530

    Sorts Q,B,I

---
+++ Sort/QuickSort.py
@@ -0,0 +1,23 @@
+def QuickSort(a):
+    if len(a) <= 1:
+        return a
+    else:
+        pivot = a.pop()
+
+    greater = []
+    lower = []
+    for i in a:
+        if i > pivot:
+            greater.append(i)
+        else:
+            lower.append(i)
+
+    return QuickSort(lower) +[pivot]+ QuickSort(greater)
+
+
+
+
+
+l = [1,2,0]
+print(QuickSort(l))
+#nlog(n)_
